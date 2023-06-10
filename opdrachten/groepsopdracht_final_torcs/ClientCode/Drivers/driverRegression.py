c547311623dbdd86c53a2501792f1e2c5696f7ea
Leandrodn <leandrodn10@gmail.com>
2023-06-10 10:25:22 +0200

    Removed extra track features

--- opdrachten/groepsopdracht_final_torcs/ClientCode/Drivers/driverRegression.py
+++ opdrachten/groepsopdracht_final_torcs/ClientCode/Drivers/driverRegression.py
@@ -26,11 +26,13 @@ class DriverRegression(DriverInterface):
     def drive(self, carState: dict) -> CommandDto:
         command = CommandDto()
         currentState = np.array([carState["speed"][0], carState["trackPos"], carState["angle"], 
-                                    carState["track"][0], carState["track"][1], carState["track"][2], carState["track"][3],
-                                    carState["track"][4], carState["track"][5], carState["track"][6], carState["track"][7],
-                                    carState["track"][8], carState["track"][9], carState["track"][10], carState["track"][11],
-                                    carState["track"][12], carState["track"][13], carState["track"][14], carState["track"][15],
-                                    carState["track"][16], carState["track"][17], carState["track"][18]])
+                                    carState["track"][4],
+                                    carState["track"][5], 
+                                    carState["track"][6], carState["track"][7], carState["track"][8], 
+                                    carState["track"][9], 
+                                    carState["track"][10], carState["track"][11], carState["track"][12],
+                                    carState["track"][13], carState["track"][14]])
+        
         
         action = self._predict([currentState])
         # print(action)
@@ -54,7 +56,7 @@ class DriverRegression(DriverInterface):
         return beta
     
     def scikitMLP(self, X, Y):
-        reg = MLPRegressor(hidden_layer_sizes=(20,20), max_iter=5000, activation='logistic', solver='adam', random_state=1)
+        reg = MLPRegressor(hidden_layer_sizes=(20,20), max_iter=500, activation='logistic', solver='adam', random_state=1)
         reg.fit(X.values, Y.values)
         return reg
 
@@ -63,18 +65,28 @@ class DriverRegression(DriverInterface):
         trainingsSet = pd.read_csv(self.trainingSetPath, 
                         sep=',', index_col=False, header=0)
         
-        print(trainingsSet)
+        print(trainingsSet.head())
 
         #clean data
         trainingsSet = self._cleanData(trainingsSet)
         trainingsSet = self._removeOutliers(trainingsSet)
 
         #Split the data into variables and results
+        # X = trainingsSet[["SPEED", "TRACK_POSITION", "ANGLE_TO_TRACK_AXIS",
+        #                          "TRACK_EDGE_0", "TRACK_EDGE_1", "TRACK_EDGE_2", "TRACK_EDGE_3", "TRACK_EDGE_4", "TRACK_EDGE_5",
+        #                           "TRACK_EDGE_6", "TRACK_EDGE_7", "TRACK_EDGE_8", "TRACK_EDGE_9", "TRACK_EDGE_10", "TRACK_EDGE_11",
+        #                            "TRACK_EDGE_12", "TRACK_EDGE_13", "TRACK_EDGE_14", "TRACK_EDGE_15", "TRACK_EDGE_16", "TRACK_EDGE_17",
+        #                              "TRACK_EDGE_18"]]
+
         X = trainingsSet[["SPEED", "TRACK_POSITION", "ANGLE_TO_TRACK_AXIS",
-                                 "TRACK_EDGE_0", "TRACK_EDGE_1", "TRACK_EDGE_2", "TRACK_EDGE_3", "TRACK_EDGE_4", "TRACK_EDGE_5",
-                                  "TRACK_EDGE_6", "TRACK_EDGE_7", "TRACK_EDGE_8", "TRACK_EDGE_9", "TRACK_EDGE_10", "TRACK_EDGE_11",
-                                   "TRACK_EDGE_12", "TRACK_EDGE_13", "TRACK_EDGE_14", "TRACK_EDGE_15", "TRACK_EDGE_16", "TRACK_EDGE_17",
-                                     "TRACK_EDGE_18"]]
+                            "TRACK_EDGE_4", "TRACK_EDGE_5",                                      #-40 degrees
+                            "TRACK_EDGE_6", "TRACK_EDGE_7", "TRACK_EDGE_8",     #-20 degrees
+                            "TRACK_EDGE_9",                                     #0 degrees
+                            "TRACK_EDGE_10", "TRACK_EDGE_11", "TRACK_EDGE_12",  #20 degrees
+                            "TRACK_EDGE_13", "TRACK_EDGE_14"]]                                       #40 degrees
+
+        print(X.head().to_string())
+
         Y = trainingsSet[["ACCELERATION", "BRAKE", "STEERING"]]
 
         # # Create linear regression object
