b7f99d84e2acf6f6c1a7f3cfa8f09a64c0cd8529
Inzui <IanZuiderent@gmail.com>
2023-06-15 15:37:14 +0200

    Added training argument

--- opdrachten/groepsopdracht_final_torcs/ClientCode/main.py
+++ opdrachten/groepsopdracht_final_torcs/ClientCode/main.py
@@ -3,20 +3,23 @@ from Services.torcsClient import TorcsClient
 from Drivers.driverRegression import DriverRegression
 from Drivers.driverPid import DriverPid
 
-def main(ipaddress: str, port: int):
+def main(ipaddress: str, port: int, training: bool):
     if (ipaddress == None):
         ipaddress = "localhost"
     if (port == None):
         port = 3001
-
+    if (training == None):
+        training = False
+        
     # client = TorcsClient(DriverPid(maxGear = 4, maxCornerSpeed = 50, brakingDistance = 90, finishAccelerationTime = 3), training = False, speedUp = False)
-    client = TorcsClient(DriverRegression(), training = False, hostname = ipaddress, port = port)
+    client = TorcsClient(DriverRegression(), training = training, hostname = ipaddress, port = port)
     client.run()
 
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser()
     argParser.add_argument("-i", "--ipaddress", type = str, help = "Torcs Server IP-Address")
     argParser.add_argument("-p", "--port", type = int, help = "Torcs Server Port")
+    argParser.add_argument("-t", "--training", type = bool, action = argparse.BooleanOptionalAction, help = "Go into training mode?")
     args = argParser.parse_args()
 
-    main(args.ipaddress, args.port)
+    main(args.ipaddress, args.port, args.training)
